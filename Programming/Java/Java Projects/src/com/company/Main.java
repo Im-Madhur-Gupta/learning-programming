4aa79e55a06a4ceb87337e78678d03126412b9d7
MADG06 <202051112@iiitvadodara.ac.in>
2021-04-14 03:09:50 +0530

    13 apr 2

--- Programming/Java/Java Projects/src/com/company/Main.java
+++ Programming/Java/Java Projects/src/com/company/Main.java
@@ -1,8 +1,10 @@
-package com.company;
+package com.company; // a package = group of classes
 
-public class Main {
+public class Main { // class = group of methods
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // main function/method, start point of execution
 	System.out.println("Hi There, I'm learning java.");
+	int a=1;
+	System.out.println(a++);
     }
 }
