16255e1fa086daa86c9aac991b328ce63aa56ced
Madhur Gupta <202051112@iiitvadodara.ac.in>
2021-05-25 09:38:09 +0530

    25

---
+++ DSA/src/vector.java
@@ -0,0 +1,10 @@
+import java.util.*;
+// above will import Vector and List classes.
+
+public class vector {
+    public static void main(String[] args) {
+        List<Employee> vector_list = new Vector<>();
+
+        vector_list.add(1,new Employee("Madhur","Gupta",123));
+    }
+}
