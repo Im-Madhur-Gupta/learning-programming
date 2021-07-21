272a089aaa9f71b009caba4ad7ce00ea82b2fda2
Madhur Gupta <202051112@iiitvadodara.ac.in>
2021-07-21 23:46:11 +0530

    jul 21

--- DSA/src/HeapSort.java
+++ DSA/src/HeapSort.java
@@ -1,17 +1,41 @@
-public class HeapSort {
-    public static void heapSortMe(int arr[]){
-        MinHeap tempheap = new MinHeap();
-        for(int i:arr){
-            tempheap.insert(i);
+public class HeapSort { // Heap's Root is at index 0.
+    public static void heapSort(int arr[]){
+        // Step 1 - convert arr to max Heap
+        int size = arr.length;
+        for(int i=(size/2)-1;i>=0;i--){
+            heapifyNode(arr,size,i);
         }
-        int i=0;
-        while(tempheap.size!=0){
-            arr[i++] = tempheap.delete();
+
+        // Step 2 - Swap Root ie index=0 with last index and decreae size by 1
+        // then apply heapify at index=0 with size (already decremented) till size==1
+        while(size!=1){
+            swapElements(arr,0,size-1);
+            size--;
+            heapifyNode(arr,size,0);
         }
     }
+    public static void heapifyNode(int arr[],int size,int tempRoot){
+        // Is function se mai TempRoot ke subTree ko (size-1) index tak heap me convert kardunga
+        // tempRoot ko pehle uske direct children se coompare karunga phir next level (if req) pe recursively jaunga.
+        int leftChild = 2*tempRoot+1, rightChild = 2*tempRoot+2;
+        if(leftChild<size && arr[leftChild]>arr[tempRoot]){
+            swapElements(arr,tempRoot,leftChild);
+            heapifyNode(arr,size,leftChild);
+        }
+        if(rightChild<size && arr[rightChild]>arr[tempRoot]){
+            swapElements(arr,tempRoot,rightChild);
+            heapifyNode(arr,size,rightChild);
+        }
+    }
+    public static void swapElements(int arr[],int index1,int index2){
+        arr[index1] = arr[index1] + arr[index2];
+        arr[index2] = arr[index1] - arr[index2];
+        arr[index1] = arr[index1] - arr[index2];
+    }
+
     public static void main(String[] args) {
-        int arr[] = {5,2,9,21,-1,2,5,0,-6};
-        heapSortMe(arr);
+        int arr[] = {5,2,9,21,-6,100,-82,0,9,21};
+        heapSort(arr);
         for(int i:arr){
             System.out.print(i+", ");
         }
--- DS_fy_Lab/src/HeapSort.java
+++ DS_fy_Lab/src/HeapSort.java
@@ -1,78 +1,32 @@
 import java.util.Scanner;
 // Madhur Gupta (202051112)
-class Heap{
-    protected int arr[];
-    protected int size; // contains number of elements in the heap. Remember this implementation has its root at index = 1.
-    protected Heap(){
-        arr = new int[10];
-        size=0;
-    }
-    protected void checkSize(){
-        if(size+1== arr.length){
-            int temparr[] = new int[arr.length*2];
-            System.arraycopy(arr,0,temparr,0,arr.length);
-            arr = temparr;
-        }
-    }
-    public void display(){
-        for(int i=1;i<size+1;i++){
-            System.out.print(arr[i]+", ");
-        }
-    }
-    public void isEmpty(){
-        if(size==0){
-            throw new IllegalArgumentException("Heap is Empty.");
-        }
-    }
-    public int peek(){
-        isEmpty();
-        return arr[1];
-    }
-}
-// This implementation works for similar as well as dissimilar data acc. to definition of Max and Min Heaps.
-class MinHeap extends Heap{
-    public void insert(int value){
-        checkSize();
-        int possibleIndex = size+1, parentIndex = (size+1) / 2;
-        while (possibleIndex != 1 && value < arr[parentIndex]) {
-            arr[possibleIndex] = arr[parentIndex];
-            possibleIndex = parentIndex;
-            parentIndex /= 2;
-        }
-        arr[possibleIndex] = value;
-        size++;
-    }
-    public int delete(){
-        isEmpty();
-        int toBeReturned = arr[1],lastElement = arr[size--],tempIndex=1;
-        while(true){
-            if(2*tempIndex<size && arr[2*tempIndex+1]<arr[2*tempIndex] && arr[2*tempIndex+1]<lastElement){
-                // L,R defined and R<L and R<value
-                arr[tempIndex]=arr[2*tempIndex+1];
-                tempIndex = 2*tempIndex+1;
-            }
-            else if(2*tempIndex<=size && arr[2*tempIndex]<lastElement) {
-                // This is for ( (L,R defined and L<R and L<value) OR (only L defined and L<value) )
-                arr[tempIndex] = arr[2 * tempIndex];
-                tempIndex *= 2;
-            }
-            else{ // In rest of the possibilities we just have to place value at tempIndex.
-                arr[tempIndex]=lastElement;
-                return toBeReturned;
-            }
-        }
-    }
-}
 public class HeapSort {
-    public static void heapSortMe(int arr[]){
-        MinHeap tempheap = new MinHeap();
-        for(int i:arr){
-            tempheap.insert(i);
+    public static void heapSort(int arr[]){
+        int size = arr.length;
+        for(int i=(size/2)-1;i>=0;i--){
+            heapifyNode(arr,size,i);
         }
-        int i=0;
-        while(tempheap.size!=0){
-            arr[i++] = tempheap.delete();
+        while(size!=1){
+            swapElements(arr,0,size-1);
+            size--;
+            heapifyNode(arr,size,0);
         }
+    }
+    public static void heapifyNode(int arr[],int size,int tempRoot){
+        int leftChild = 2*tempRoot+1, rightChild = 2*tempRoot+2;
+        if(leftChild<size && arr[leftChild]>arr[tempRoot]){
+            swapElements(arr,tempRoot,leftChild);
+            heapifyNode(arr,size,leftChild);
+        }
+        if(rightChild<size && arr[rightChild]>arr[tempRoot]){
+            swapElements(arr,tempRoot,rightChild);
+            heapifyNode(arr,size,rightChild);
+        }
+    }
+    public static void swapElements(int arr[],int index1,int index2){
+        arr[index1] = arr[index1] + arr[index2];
+        arr[index2] = arr[index1] - arr[index2];
+        arr[index1] = arr[index1] - arr[index2];
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -82,10 +36,9 @@ public class HeapSort {
         for(int i =0 ;i < strArr.length;i++){
             arr[i]= Integer.parseInt(strArr[i]);
         }
-        heapSortMe(arr);
-        System.out.println("\nSorted sequence is -");
-        for(int j:arr){
-            System.out.print(j+"  ");
+        heapSort(arr);
+        for(int i:arr){
+            System.out.print(i+", ");
         }
     }
 }
